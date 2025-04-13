a3a0e6af92e6335365db3489b727afc4a3f8a5b2
Renaud Malaval <renaud.malaval@free.fr>
2025-04-13 14:55:11 +0200

    replace the 4 strings to version in datam.rc file to define it in resource.h

--- datam/resource.h
+++ datam/resource.h
@@ -16,12 +16,16 @@
 // Next default values for new objects
 //
 #ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
+    #ifndef APSTUDIO_READONLY_SYMBOLS
+        #define _APS_NO_MFC             130
+        #define _APS_NEXT_RESOURCE_VALUE 129
+        #define _APS_NEXT_COMMAND_VALUE  32771
+        #define _APS_NEXT_CONTROL_VALUE  1000
+        #define _APS_NEXT_SYMED_VALUE    110
+    #endif
+#endif
 
-#define _APS_NO_MFC             130
-#define _APS_NEXT_RESOURCE_VALUE 129
-#define _APS_NEXT_COMMAND_VALUE  32771
-#define _APS_NEXT_CONTROL_VALUE  1000
-#define _APS_NEXT_SYMED_VALUE    110
-#endif
-#endif
+#define FILE_VERSION_COMMA      1,11,12,29
+#define PRODUCT_VERSION_COMMA   FILE_VERSION_COMMA
+#define FILE_VERSION_DOT        "1.11.12.29\0"
+#define PRODUCT_VERSION_DOT     FILE_VERSION_DOT
